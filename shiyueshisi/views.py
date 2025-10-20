45a19ab4b04996a74476e6f7092c451f2a79d633
王胜泽 <18134193323@163.com>
2025-10-20 21:07:11 +0800

    Merge remote-tracking branch 'origin/main'

--- shiyueshisi/views.py
+++ shiyueshisi/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def hello(request):
-    return HttpResponse(b"Hello, World! This is a simple Django app.")
+    return HttpResponse(b"Hello, World1111! This is a simple Django app.")
